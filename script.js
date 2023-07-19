b6d0795604620880d88c04bc0c728ecd3a74135e
ishu-00 <ishikadahiya22@gmail.com>
2023-07-19 23:57:50 +0530

    Updated Weather Location: accessing user's location with JS

--- script.js
+++ script.js
@@ -35,6 +35,57 @@ let weather = {
   },
 };
 
+let geocode ={
+  reverseGeocode: function (latitude, longitude){
+      var api_key = '9c12860169c24746b594457ddee130b0';
+    
+      var query = latitude + ',' + longitude;
+      var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+    
+      var request_url = api_url
+        + '?'
+        + 'key=' + api_key
+        + '&q=' + encodeURIComponent(query)
+        + '&pretty=1'
+        + '&no_annotations=1';
+    
+      var request = new XMLHttpRequest();
+      request.open('GET', request_url, true);
+    
+      request.onload = function() {
+    
+        if (request.status === 200){
+          var data = JSON.parse(request.responseText);
+          weather.fetchWeather(data.results[0].components.city);
+        } else if (request.status <= 500){
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log('error msg: ' + data.status.message);
+        } else {
+          console.log("server error");
+        }
+      };
+    
+      request.onerror = function() {
+        console.log("unable to connect to server");
+      };
+    
+      request.send();
+  },
+
+  getlocation: function(){
+    function success(data){
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if(navigator.geolocation){
+     navigator.geolocation.getCurrentPosition(success, console.error);
+    }
+    else {
+    weather.search();
+    }
+  }
+}
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
@@ -46,3 +97,5 @@ document
       weather.search();
     }
   });
+
+  geocode.getlocation();
