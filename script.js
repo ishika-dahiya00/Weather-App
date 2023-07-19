3f6c5f4abc27a79d8811c2eff2f9bcff865e717d
ishu-00 <ishikadahiya22@gmail.com>
2023-07-19 19:57:56 +0530

    Added Search and Keypress function, made changes in CSS and JS

--- script.js
+++ script.js
@@ -8,19 +8,41 @@ let weather = {
         this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
+      .then((data) => this.displayWeather(data)).catch(error=>alert("City Entered Not Found"));
   },
   displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
-    const {speed} = data.wind;
-    console.log(name,icon,description,temp,humidity,speed)
-  document.querySelector(".city").innerText = " Weather in " + name;
-  document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
-  document.querySelector(".desc").innerText = description;
-  document.querySelector(".temp").innerText = temp + "°C";
-  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-  document.querySelector(".wind").innerText = "Wind Speed: "+ speed + " Km/h";
-},
+    const { speed } = data.wind;
+    // console.log(name,icon,description,temp,humidity,speed)
+    document.querySelector(".city").innerText = " Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".desc").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+    "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+    "Wind Speed: " + speed + " Km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+    "url('https://source.unsplash.com/1600x900/?" + name + "' )";
+  },
+
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
